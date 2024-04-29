b697b74aaaad209693a03f9c9cab0a99714bda60
tom38110 <tom.audard@gmail.com>
2024-04-29 22:33:54 +0200

    avis

--- Front/pieces.js
+++ Front/pieces.js
@@ -1,3 +1,4 @@
+import { ajoutListenersAvis } from "./avis.js";
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
